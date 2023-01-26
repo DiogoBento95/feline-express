aeaae4a208ddad74d5d2d64c43329a72b98c8633
Diogo Bento <diogo.bento@bringglobal.com>
2023-01-26 22:56:32 -0100

    feat: Generation and Routing of HomeComponent.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['../../scss/2-components/_home.scss']
+})
+export class HomeComponent {
+
+}
