a59d8ce0f1905e149a2c54e4779c20e9380fa16a
Diogo Bento <diogo.bento@bringglobal.com>
2023-02-16 19:28:24 -0100

    feat: Creating the ContactComponent Form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AdoptComponent } from './adopt/adopt.component';
@@ -18,6 +19,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['../../scss/2-components/_contact.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  felineForm!: FormGroup;
+
+  constructor(private router: Router) {}
+
+  private initForm() {
+    let formName = '';
+    let formEmail = '';
+    let formSubject = '';
+    let formDescription = '';
+
+    this.felineForm = new FormGroup({
+      'name': new FormControl(formName, Validators.required),
+      'email': new FormControl(formEmail, [Validators.email, Validators.required]),
+      'subject': new FormControl(formSubject, Validators.required),
+      'description': new FormControl(formDescription, Validators.required),
+    });
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  //ngSubmit - HTML
+  onSubmit() {
+    console.log(this.felineForm);
+  }
+
+  onCancel() {
+    this.felineForm.reset();
+    this.router.navigate(['']);
+  }
 
 }
